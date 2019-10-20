ae39bbae14139e5e8332bb13272096a0d95739a7
Mary0806 <54910489+Mary0806@users.noreply.github.com>
2019-10-20 11:23:15 +0300

    Create 108. Convert Sorted Array to Binary Search Tree.cpp

---
+++ 108. Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,100 @@
+///https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() <= 0)
+		return NULL;
+	if (nums.size() == 1)
+	{
+		return new TreeNode(nums[0]);
+	}
+	int n = nums.size();
+	
+	return f(0, n - 1, nums);
+}
+TreeNode* f(int a, int b, vector<int> nums)
+{
+	if (a > b)
+		return NULL;
+	if (a == b)
+		return new TreeNode(nums[a]);
+	int c =  a + (b - a) / 2;
+	TreeNode* root = new TreeNode(nums[c]);
+	root->left = f(a, c - 1, nums);
+	root->right = f(c + 1, b,  nums);
+	return root;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string treeNodeToString(TreeNode* root) {
+    if (root == nullptr) {
+      return "[]";
+    }
+
+    string output = "";
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+          output += "null, ";
+          continue;
+        }
+
+        output += to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        TreeNode* ret = Solution().sortedArrayToBST(nums);
+
+        string out = treeNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
