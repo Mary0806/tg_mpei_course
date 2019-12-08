e51d8f983876fd1d117155017e003d29ac219516
Mary0806 <54910489+Mary0806@users.noreply.github.com>
2019-12-08 18:43:51 +0300

    Create 210. Course Schedule II.cpp

---
+++ 210. Course Schedule II.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/course-schedule-ii/submissions/
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>res;
+            vector<int> a (numCourses);
+                vector<vector<int>>graph(numCourses);
+        
+        for(auto p: prerequisites){
+            graph[p[1]].push_back(p[0]);
+            a[p[0]]++;
+        }
+        
+        for(int i = 0; i < numCourses; i++){
+            for(int j=0; j < numCourses; j++)
+                if(a[j] == 0)
+                    break;
+                 if (j == numCourses)
+                     return vector<int>();
+                    a[j] = -1;
+                 for(auto x: graph[j]) 
+                     a[x]--;
+            res.push_back(j);
+        }
+        return res;  
+    }
+};
